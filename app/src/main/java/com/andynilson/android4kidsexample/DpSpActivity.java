13367802330f0356d378d3b1d7cc864aaba9bd0f
Andy Nilson <andy@andynilson.com>
2016-02-12 21:48:33 -0800

    initial add

---
+++ app/src/main/java/com/andynilson/android4kidsexample/DpSpActivity.java
@@ -0,0 +1,14 @@
+package com.andynilson.android4kidsexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DpSpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dp_sp);
+    }
+
+}
---
+++ app/src/main/java/com/andynilson/android4kidsexample/MainActivity.java
@@ -0,0 +1,41 @@
+package com.andynilson.android4kidsexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void navigateToActivity(View view) {
+        Intent intent = null;
+
+        if (view.getId() == R.id.dpsp_button) {
+            intent = new Intent(this, DpSpActivity.class);
+        } else if (view.getId() == R.id.wrapped_match_button) {
+            intent = new Intent(this, WrappedMatchActivity.class);
+        } else if (view.getId() == R.id.orientationhorizontal_button) {//
+            intent = new Intent(this, OrientationHorizontalActivity.class);//
+        } else if (view.getId() == R.id.orientationvertical_button) {//
+            intent = new Intent(this, OrientationVerticalActivity.class);//
+        }
+
+
+        if (intent == null) {
+            Log.e(TAG, "bad view for navigation" + view);
+            //TODO toast
+            return;
+        }
+
+        startActivity(intent);
+    }
+
+}
